c3539fb2d2507c98901213919ac5b6a019ce1151
MiguelMDutra <miguelnewgen@gmail.com>
2026-05-13 15:02:49 -0300

    adiciona projeto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import "./header.css";
+
+export function Header() {
+  return (
+    <section className="header--section">
+      <div className="header--name">Miguel M Dutra</div>
+
+      <div>
+        <a>sobre</a>
+        <a>projetos</a>
+        <a>experiência</a>
+      </div>
+
+      <div>
+        <a>contato</a>
+      </div>
+    </section>
+  );
+}
